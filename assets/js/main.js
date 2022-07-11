586343d4dc1d75abc019774eafe04e0642b8c9b0
Nilupul Madhawa <dnmadawa@gmail.com>
2022-07-11 22:17:51 +0530

    coming soon page

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+
+
+window.onload = function () {
+  var preloader = document.getElementsByClassName('preloader')[0];
+  setTimeout(function () {
+    preloader.style.display = 'none';
+  }, 2000);
+};
